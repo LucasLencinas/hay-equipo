bc8eddca5544eaf79c7ba742f65d9efc62d014a3
Lucas <lllencinas@gmail.com>
2016-03-12 20:05:05 -0300

    agrupo lo que tengo hasta ahora del frontend

---
+++ public/data1.js
@@ -0,0 +1,249 @@
+var partidos = [
+	{
+		fecha: new Date(2016, 00, 07),
+		equipo1: ["Marian", "Colo", "Martin", "Alan", "Gaby"],
+		equipo2: ["Lucas","Jony","Gorra","Pablote","Agus O"],
+		diferenciaGoles: 2
+	},
+	{
+		fecha: new Date(2016, 00, 14),
+		equipo1: ["Gaby", "Alan", "Marian", "Agus O", "Martin"],
+		equipo2: ["Cabe","Agus L","Jony","Gorra","Pablote"],
+		diferenciaGoles: 1
+	},
+	{
+		fecha: new Date(2016, 00, 21),
+		equipo1: ["Marian", "Colo", "Fede", "Martin", "Agus O"],
+		equipo2: ["Agus L","Jony","Gorra","Pablote","Alan"],
+		diferenciaGoles: 5
+	},
+	{
+		fecha: new Date(2016, 00, 28),
+		equipo1: ["Fede", "Carda", "Gaby", "Alan", "Martin", "Gorra"],
+		equipo2: ["Colo","Jony","Cabe","Pablote","Agus O", "Agus L"],
+		diferenciaGoles: 4
+	}
+];
+
+var jugadores = [
+	{
+		nombre: "Lucas",
+		edad: 23,
+		peso: 80,
+		habilidades: {
+			ARQ: 22,
+			DEF: 44,
+			MED: 66,
+			DEL: 88
+		}	
+	},
+	{
+		nombre: "Colo",
+		edad: 25,
+		peso: 80,
+		habilidades: {
+			ARQ: 22,
+			DEF: 44,
+			MED: 66,
+			DEL: 88
+		}
+	},
+	{
+		nombre: "Marian",
+		edad: 23,
+		peso: 80,
+		habilidades: {
+			ARQ: 22,
+			DEF: 44,
+			MED: 66,
+			DEL: 88
+		}	
+	},
+	{
+		nombre: "Mati",
+		edad: 25,
+		peso: 80,
+		habilidades: {
+			ARQ: 22,
+			DEF: 44,
+			MED: 66,
+			DEL: 88
+		}
+	},
+	{
+		nombre: "Gaby",
+		edad: 23,
+		peso: 80,
+		habilidades: {
+			ARQ: 22,
+			DEF: 44,
+			MED: 66,
+			DEL: 88
+		}	
+	},
+	{
+		nombre: "Manu",
+		edad: 25,
+		peso: 80,
+		habilidades: {
+			ARQ: 22,
+			DEF: 44,
+			MED: 66,
+			DEL: 88
+		}
+	},
+	{
+		nombre: "Agus O",
+		edad: 23,
+		peso: 80,
+		habilidades: {
+			ARQ: 22,
+			DEF: 44,
+			MED: 66,
+			DEL: 88
+		}	
+	},
+	{
+		nombre: "Pablito",
+		edad: 25,
+		peso: 80,
+		habilidades: {
+			ARQ: 22,
+			DEF: 44,
+			MED: 66,
+			DEL: 88
+		}
+	},
+	{
+		nombre: "Pablote",
+		edad: 23,
+		peso: 80,
+		habilidades: {
+			ARQ: 22,
+			DEF: 44,
+			MED: 66,
+			DEL: 88
+		}	
+	},
+	{
+		nombre: "Carda",
+		edad: 25,
+		peso: 80,
+		habilidades: {
+			ARQ: 22,
+			DEF: 44,
+			MED: 66,
+			DEL: 88
+		}
+	},
+	{
+		nombre: "Cristobal",
+		edad: 23,
+		peso: 80,
+		habilidades: {
+			ARQ: 22,
+			DEF: 44,
+			MED: 66,
+			DEL: 88
+		}	
+	},
+	{
+		nombre: "Fede",
+		edad: 25,
+		peso: 80,
+		habilidades: {
+			ARQ: 22,
+			DEF: 44,
+			MED: 66,
+			DEL: 88
+		}
+	},
+	{
+		nombre: "Agus L",
+		edad: 23,
+		peso: 80,
+		habilidades: {
+			ARQ: 22,
+			DEF: 44,
+			MED: 66,
+			DEL: 88
+		}	
+	},
+	{
+		nombre: "Martin",
+		edad: 25,
+		peso: 80,
+		habilidades: {
+			ARQ: 22,
+			DEF: 44,
+			MED: 66,
+			DEL: 88
+		}
+	},
+	{
+		nombre: "Jony",
+		edad: 23,
+		peso: 80,
+		habilidades: {
+			ARQ: 22,
+			DEF: 44,
+			MED: 66,
+			DEL: 88
+		}	
+	},
+	{
+		nombre: "Gorra",
+		edad: 25,
+		peso: 80,
+		habilidades: {
+			ARQ: 22,
+			DEF: 44,
+			MED: 66,
+			DEL: 88
+		}
+	},
+	{
+		nombre: "Alan",
+		edad: 23,
+		peso: 80,
+		habilidades: {
+			ARQ: 22,
+			DEF: 44,
+			MED: 66,
+			DEL: 88
+		}	
+	},
+	{
+		nombre: "Cabe",
+		edad: 25,
+		peso: 80,
+		habilidades: {
+			ARQ: 22,
+			DEF: 44,
+			MED: 66,
+			DEL: 88
+		}
+	},
+	{
+		nombre: "CoLucas",
+		edad: 23,
+		peso: 80,
+		habilidades: {
+			ARQ: 22,
+			DEF: 44,
+			MED: 66,
+			DEL: 88
+		}	
+	},
+	{
+		nombre: "Colhacha",
+		edad: 25,
+		peso: 80,
+		habilidades: {
+			ARQ: 22,
+			DEF: 44,
+			MED: 66,
+			DEL: 88
+		}
+	},
+	]
