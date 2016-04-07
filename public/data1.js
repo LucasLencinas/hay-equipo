71d0b6163053cde006767a1597b8122b2c17e723
LucasLencinas <luucas.lencinas@gmail.com>
2016-04-07 19:30:48 +0000

    agrego datos del 2015. faltan los datos de los que no juegan. capaz que habria que agregarlos

--- public/data1.js
+++ public/data1.js
@@ -1,26 +1,63 @@
 var partidos = [
 	{
-		fecha: new Date(2016, 00, 07),
-		equipo1: ["Marian", "Colo", "Martin", "Alan", "Gaby"],
-		equipo2: ["Lucas","Jony","Gorra","Pablote","Agus O"],
+		fecha: 20160107,
+		equipo1: ['Marian', 'Colo', 'Martin', 'Alan', 'Gaby'],
+		equipo2: ['Lucas', 'Jony', 'Pablote', 'Agus O', 'GoResta'],
 		diferenciaGoles: 2
-	},
-	{
-		fecha: new Date(2016, 00, 14),
-		equipo1: ["Gaby", "Alan", "Marian", "Agus O", "Martin"],
-		equipo2: ["Cabe","Agus L","Jony","Gorra","Pablote"],
+	},{	
+		fecha: 20160114,
+		equipo1: ['Gaby', 'Alan', 'Marian', 'Agus O', 'Martin'],
+		equipo2: ['Cabe', 'Agus L', 'GoResta', 'Pablote', 'Jony'],
 		diferenciaGoles: 1
-	},
-	{
-		fecha: new Date(2016, 00, 21),
-		equipo1: ["Marian", "Colo", "Fede", "Martin", "Agus O"],
-		equipo2: ["Agus L","Jony","Gorra","Pablote","Alan"],
+	},{	
+		fecha: 20160121,
+		equipo1: ['Marian', 'Colo', 'Fede', 'Martin', 'Agus O'],
+		equipo2: ['Jony', 'Agus L', 'Pablote', 'GoResta', 'Alan'],
 		diferenciaGoles: 5
-	},
-	{
-		fecha: new Date(2016, 00, 28),
-		equipo1: ["Fede", "Carda", "Gaby", "Alan", "Martin", "Gorra"],
-		equipo2: ["Colo","Jony","Cabe","Pablote","Agus O", "Agus L"],
+	},{	
+		fecha: 20160128,
+		equipo1: ['Fede', 'Carda', 'Gaby', 'Martin', 'GoResta'],
+		equipo2: ['Colo', 'Jony', 'Pablote', 'Agus O', 'Cabe'],
+		diferenciaGoles: 4
+	},{	
+		fecha: 20160204,
+		equipo1: ['Carda', 'Martin', 'Pablote', 'Agus L', 'Gaby'],
+		equipo2: ['Jony', 'Fede', 'Colo', 'Agus O', 'GoResta'],
+		diferenciaGoles: 8
+	},{	
+		fecha: 20160211,
+		equipo1: ['Gaby', 'Jony', 'Martin', 'Fede', 'GoResta'],
+		equipo2: ['Agus O', 'Agus L', 'Colo', 'Alan', 'Pablote'],
+		diferenciaGoles: 5
+	},{	
+		fecha: 20160218,
+		equipo1: ['Mati', 'Lucas', 'Fede', 'Jony', 'Martin'],
+		equipo2: ['Cabe', 'Carda', 'Agus L', 'GoResta', 'Colo'],
+		diferenciaGoles: 1
+	},{	
+		fecha: 20160225,
+		equipo1: ['Agus O', 'Colo', 'CoLucas', 'CoLuNacho', 'Martin'],
+		equipo2: ['Gaby', 'Agus L', 'Jony', 'GoResta', 'Fede'],
+		diferenciaGoles: 7
+	},{	
+		fecha: 20160303,
+		equipo1: ['Mati', 'Colo', 'Jony', 'Martin', 'Agus O'],
+		equipo2: ['Cabe', 'Agus L', 'Alan', 'GoResta', 'Gaby'],
+		diferenciaGoles: 7
+	},{	
+		fecha: 20160310,
+		equipo1: ['Mati', 'Marian', 'Agus L', 'Cabe', 'Martin'],
+		equipo2: ['GoResta', 'Lucas', 'Alan', 'LukAle', 'Jony'],
+		diferenciaGoles: 0
+	},{	
+		fecha: 20160317,
+		equipo1: ['GoResta', 'LucaFa', 'Agus O', 'Fede', 'Lucas'],
+		equipo2: ['Gaby', 'Agus L', 'Martin', 'Marian', 'Cabe'],
+		diferenciaGoles: 5
+	},{	
+		fecha: 20160331,
+		equipo1: ['Carda', 'Agus L', 'Gaby', 'Cabe', 'Colo'],
+		equipo2: ['Lucas', 'Marian', 'GoResta', 'Agus O', 'Martin'],
 		diferenciaGoles: 4
 	}
 ];
